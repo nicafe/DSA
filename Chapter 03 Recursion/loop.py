8a3da3f880b886a49f7e128d335cd008b213c6e7
Nicolas Zhang <zhangjizheng54@hotmail.com>
2025-02-19 10:10:45 +0800

    Add recursion examples

--- Chapter 03 Recursion/loop.py
+++ Chapter 03 Recursion/loop.py
@@ -1,12 +1,52 @@
-def print_to_loop(n):
+def factorial_loop(n):
+    res = 1
+    for i in range(2, n + 1):
+        res *= i
+    return res
+
+
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+def output_loop(n):
     for i in range(n + 1):
         print(i)
 
 
-def print_to(n):
-    if n != 0:
-        print_to(n - 1)
+def output(n):
+    if n > 0:
+        output(n - 1)
     print(n)
 
-print_to_loop(5)
-print_to(5)
+
+def output_pos(n, i=0):
+    if n > 0:
+        output_pos(n - 1, i + 1)
+    print('  ' * i + str(n))
+
+
+def output_rev(n, i=0):
+    print('  ' * i + str(n))
+    if n > 0:
+        output_rev(n - 1, i + 1)
+
+
+def output_pos_rev(n, i=0):
+    print('  ' * i + str(n))
+    if n > 0:
+        output_pos_rev(n - 1, i + 1)
+    print('  ' * i + str(n))
+
+
+# print(factorial_loop(5))
+# print(factorial(5))
+
+# output_loop(5)
+# output(5)
+
+# output_pos(5)
+# output_rev(5)
+output_pos_rev(5)
