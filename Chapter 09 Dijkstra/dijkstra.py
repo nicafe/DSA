c0b4458a3c48b57a1ac6fcd59b8717b9415020c5
Nicolas Zhang <zhangjizheng54@hotmail.com>
2025-02-25 20:23:33 +0800

    Refacter dijkstra.py

--- Chapter 09 Dijkstra/dijkstra.py
+++ Chapter 09 Dijkstra/dijkstra.py
@@ -1,16 +1,15 @@
 import math
 
 
-def dijkstra(graph):
+def dijkstra(graph, start='start'):
+    # Initiate a result dictionary where res[node] = (cost, parent).
+    res = {}
+    for node in graph:
+        res[node] = (math.inf, None)
+    res[start] = (0, None)
+    
     # Use a copied graph to keep the original one unchanged.
     copied_graph = graph.copy()
-    # Initiate result.
-    res = {}
-    for node, cost in copied_graph.pop('start').items():
-        res[node] = (cost, 'start')
-    for node in copied_graph:
-        if node not in res:
-            res[node] = (math.inf, None)
     
     while copied_graph:
         # Find the lowest cost node among the remaining nodes.
@@ -30,11 +29,12 @@ def dijkstra(graph):
     return res
 
 
-def print_path(target, res, entry=True):
-    if target == 'start':
-        print('The shortest path: start', end='')
+def print_path(res, target='end', entry=True):
+    parent = res[target][1]
+    if parent is None:
+        print(f'The shortest path: {target}', end='')
     else:
-        print_path(res[target][1], res, False)
+        print_path(res, parent, False)
         print(f' -> {target}', end='')
     if entry:
         print()
@@ -43,24 +43,42 @@ def print_path(target, res, entry=True):
 # Build the graph.
 graph = {}
 
+# Start to End.
 graph['start'] = {}
 graph['start']['a'] = 6
-graph["start"]["b"] = 2
-
+graph['start']['b'] = 2
 graph['a'] = {}
 graph['a']['end'] = 1
-
 graph['b'] = {}
 graph['b']['a'] = 3
 graph['b']['end'] = 5
-
 graph['end'] = {}
+start = 'start'
+target = 'end'
+
+# # Book to Piano.
+# graph['book'] = {}
+# graph['book']['LP'] = 5
+# graph['book']['poster'] = 0
+# graph['poster'] = {}
+# graph['poster']['drums'] = 35
+# graph['poster']['bass'] = 30
+# graph['LP'] = {}
+# graph['LP']['drums'] = 20
+# graph['LP']['bass'] = 15
+# graph['bass'] = {}
+# graph['bass']['piano'] = 20
+# graph['drums'] = {}
+# graph['drums']['piano'] = 10
+# graph['piano'] = {}
+# start = 'book'
+# target = 'piano'
 
 # Print the result.
-res = dijkstra(graph)
+res = dijkstra(graph, start)
 print('The result dictionary:')
 for k, v in res.items():
     print(k, v)
 print()
-print(f"The total length of the shortest path: {res['end'][0]}")
-print_path('end', res)
+print(f"The total length of the shortest path: {res[target][0]}")
+print_path(res, target)
